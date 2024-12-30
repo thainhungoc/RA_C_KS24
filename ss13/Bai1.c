8c982530c3bad5acac1723f2bda71f191fc4f291
TNNgoc <150165535+thainhungoc@users.noreply.github.com>
2024-12-30 19:35:00 +0700

    commit

---
+++ ss13/Bai1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define MAX 100
+
+void taoMaTran(int maTran[][MAX], int hang, int cot) {
+    printf("Nhap gia tri cho ma tran:\n");
+    for(int i = 0; i < hang; i++) {
+        for(int j = 0; j < cot; j++) {
+            printf("Nhap phan tu [%d][%d]: ", i, j);
+            scanf("%d", &maTran[i][j]);
+        }
+    }
+}
+
+void hienThiMaTran(int maTran[][MAX], int hang, int cot) {
+    printf("\nMa tran da nhap:\n");
+    for(int i = 0; i < hang; i++) {
+        for(int j = 0; j < cot; j++) {
+            printf("%d\t", maTran[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int maTran[MAX][MAX];
+    int hang, cot;
+
+    printf("Nhap so hang: ");
+    scanf("%d", &hang);
+    printf("Nhap so cot: ");
+    scanf("%d", &cot);
+
+    if(hang <= 0 || cot <= 0 || hang > MAX || cot > MAX) {
+        printf("So hang va so cot phai lon hon 0 va nho hon %d!\n", MAX);
+        return 1;
+    }
+
+    taoMaTran(maTran, hang, cot);
+    hienThiMaTran(maTran, hang, cot);
+    
+    return 0;
+}
---
+++ ss13/Bai2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int timUCLN(int a, int b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    
+    return a;
+}
+
+int main() {
+    int so1, so2;
+    
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &so1);
+    printf("Nhap so thu hai: ");
+    scanf("%d", &so2);
+    
+    if (so1 == 0 && so2 == 0) {
+        printf("UCLN khong xac dinh vi ca hai so deu bang 0!\n");
+        return 1;
+    }
+    
+    int ucln = timUCLN(so1, so2);
+    printf("Uoc chung lon nhat cua %d va %d la: %d\n", so1, so2, ucln);
+    
+    return 0;
+}
